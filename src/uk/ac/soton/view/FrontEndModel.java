968bbbae4335eb1741e3f360d28f0b3edc4af008
Christian Folkesson <christian.uaf@gmail.com>
2019-02-28 12:58:00 +0000

    Selected runways now display stopway and clearway

--- src/uk/ac/soton/view/FrontEndModel.java
+++ src/uk/ac/soton/view/FrontEndModel.java
@@ -8,23 +8,31 @@ import java.util.Set;
 public class FrontEndModel {
 
     private Map<String, Point> runwayPositions;
-    private Map<String,Dimension> runwayDimensions;
+    private Map<String, Dimension> runwayDimensions;
+    private Map<String, Dimension> runwayStopway;
+    private Map<String, Dimension> runwayClearway;
 
     FrontEndModel(){
         runwayDimensions = new HashMap<>();
         runwayPositions = new HashMap<>();
+        runwayStopway = new HashMap<>();
+        runwayClearway = new HashMap<>();
         populateModel();
     }
 
     //Debug function used to generate some test data.
     private void populateModel(){
         String runway1 = "07L";
-        runwayPositions.put(runway1, new Point(-400,0));
+        runwayPositions.put(runway1, new Point(-400,100));
         runwayDimensions.put(runway1, new Dimension(1200,80));
+        runwayStopway.put(runway1, new Dimension(200,80));
+        runwayClearway.put(runway1, new Dimension( 400,120));
 
         String runway2 = "11L";
         runwayPositions.put(runway2, new Point(-800,-500));
         runwayDimensions.put(runway2, new Dimension(2000,100));
+        runwayStopway.put(runway2, new Dimension(300,100));
+        runwayClearway.put(runway2, new Dimension( 500,150));
     }
 
     //Returns a set of strings representing runways.
@@ -46,5 +54,15 @@ public class FrontEndModel {
     public Integer getBearing(String runwayId){
         Integer bearing = Integer.parseInt(runwayId.substring(0,2))*10;
         return bearing;
+    }
+
+    //Returns the Dimension of a given runway's stopway.
+    public Dimension getStopwayDim(String runwayId){
+        return  runwayStopway.get(runwayId);
+    }
+
+    //Returns the Dimensions of a given runway's clearway.
+    public Dimension getClearwayDim(String runwayId){
+        return runwayClearway.get(runwayId);
     }
 }
--- src/uk/ac/soton/view/TopView2D.java
+++ src/uk/ac/soton/view/TopView2D.java
@@ -7,6 +7,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.util.Set;
 
 //Represents a JPanel designed to view a top-down view of the runways.
 public class TopView2D extends JPanel {
@@ -21,30 +22,21 @@ public class TopView2D extends JPanel {
     private Point globalPan;
     private Double globalZoom;
 
-    //Default values used throughout the class.
-    private final Integer CENTERLINE_PADDING = 20;
-    private final Integer SELECTED_RUNWAY_HIGHLIGHT_WIDTH = 3;
-    private final Double MAX_ZOOM_FACTOR = 5.0;
-    private final Double MIN_ZOOM_FACTOR = 0.05;
-    private final Dimension DEFAULT_VIEW_SIZE = new Dimension(800,800);
-    private final Point DEFAULT_PAN = new Point(DEFAULT_VIEW_SIZE.width/2, DEFAULT_VIEW_SIZE.height/2);
-    private final Double DEFAULT_ZOOM = 0.5;
-
-
     TopView2D(AppView appView, FrontEndModel model, MenuPanel menuPanel){
         this.appView = appView;
         this.model = model;
         this.menuPanel = menuPanel;
-        this.globalPan = new Point(DEFAULT_PAN);
-        this.globalZoom = new Double(DEFAULT_ZOOM);
+        this.globalPan = Settings.TOP_DOWN_DEFAULT_PAN;
+        this.globalZoom = Settings.TOP_DOWN_DEFAULT_ZOOM;
 
-        this.setPreferredSize(DEFAULT_VIEW_SIZE);
+        this.setPreferredSize(Settings.TOP_DOWN_DEFAULT_SIZE);
         PanAndZoomListener panListener = new PanAndZoomListener();
         this.addMouseWheelListener(panListener);
         this.addMouseListener(panListener);
         this.addMouseMotionListener(panListener);
     }
 
+    @Override
     protected void paintComponent(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
 
@@ -54,7 +46,7 @@ public class TopView2D extends JPanel {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 
         //Set the background colour.
-        g2.setColor(Color.DARK_GRAY);
+        g2.setColor(Settings.AIRFIELD_COLOUR);
         g2.fillRect(0,0,getWidth(),getHeight());
 
         //Configure the graphic's transformation to account for pan and zoom.
@@ -68,6 +60,7 @@ public class TopView2D extends JPanel {
 
     }
 
+    //Paints all main components of the UI.
     private void paintView(Graphics2D g2){
         boolean isIsolated = menuPanel.isIsolateMode();
         boolean isRunwaySelected = !(appView.getSelectedRunway() == "");
@@ -84,91 +77,40 @@ public class TopView2D extends JPanel {
         paintSelectedRunway(g2);
     }
 
-    //Configures the specified graphics object such that pan and zoom are taken into account.
-    private void configureGlobalTransform(Graphics2D g2){
-        //Create a global affine transformation which pans and zooms the view accordingly.
-        AffineTransform globalTransform = new AffineTransform();
-
-        //Translate and scale the view to match the pan and zoom settings.
-        globalTransform.translate(globalPan.x, globalPan.y);
-        globalTransform.scale(globalZoom,globalZoom);
-
-        //Set the create transformation to the one used by the image.
-        g2.setTransform(globalTransform);
-    }
-
-    //Draws a set of axis which intersect at (0,0).
-    private void paintAxis(Graphics2D g2){
-        g2.setColor(new Color(101, 101, 101));
-        g2.setStroke(new BasicStroke(1));
-        g2.drawLine(-10000,0,10000,0);
-        g2.drawLine(0,-10000,0,10000);
-    }
-
-    //Draws a runway given the name.
-    private void paintRunway(String id, Graphics2D g2){
-        g2.setColor(Color.GRAY);
+    //Paints an outline of the clearway for a specified runway.
+    private void paintClearway(String id,Graphics2D g2){
         Point pos = model.getRunwayPos(id);
-        Dimension dim = model.getRunwayDim(id);
+        Dimension runwayDim = model.getRunwayDim(id);
+        Dimension clearwayDim = model.getClearwayDim(id);
 
         AffineTransform old = g2.getTransform();
         AffineTransform tx = (AffineTransform) old.clone();
-        tx.concatenate(createRunwayTransform(pos,dim,id));
+        tx.concatenate(createRunwayTransform(pos,runwayDim,id));
         g2.setTransform(tx);
 
-        g2.fillRect(pos.x, pos.y, dim.width, dim.height);
+        g2.setColor(Settings.CLEARWAY_COLOUR);
+        g2.setStroke(Settings.CLEARWAY_STROKE);
+        g2.drawRect(pos.x+runwayDim.width, pos.y-(clearwayDim.height-runwayDim.height)/2,
+                clearwayDim.width, clearwayDim.height);
         g2.setTransform(old);
     }
 
-    //Draws the centerline for a runway given the name.
-    private void paintCenterline(String id, Graphics2D g2){
-        g2.setColor(Color.WHITE);
-        g2.setStroke(new BasicStroke(3,BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 10,new float[] {15,10} , 1));
+    //Paints an outline of the stopway for a specified runway.
+    private void paintStopway(String id,Graphics2D g2){
         Point pos = model.getRunwayPos(id);
-        Dimension dim = model.getRunwayDim(id);
+        Dimension runwayDim = model.getRunwayDim(id);
+        Dimension stopwayDim = model.getStopwayDim(id);
 
         AffineTransform old = g2.getTransform();
         AffineTransform tx = (AffineTransform) old.clone();
-        tx.concatenate(createRunwayTransform(pos,dim,id));
+        tx.concatenate(createRunwayTransform(pos,runwayDim,id));
         g2.setTransform(tx);
 
-        //Paint he centerline for the respective runway.
-        g2.drawLine(pos.x + CENTERLINE_PADDING, pos.y + dim.height/2, pos.x + dim.width - CENTERLINE_PADDING, pos.y + dim.height/2);
+        g2.setColor(Settings.STOPWAY_COLOUR);
+        g2.setStroke(Settings.STOPWAY_STROKE);
+        g2.drawRect(pos.x+runwayDim.width, pos.y-(stopwayDim.height-runwayDim.height)/2,
+                stopwayDim.width, stopwayDim.height);
         g2.setTransform(old);
-    }
-
-    //Draws the runway & centerline for all runways in the current model.
-    private void paintRunways(Graphics2D g2){
-        for(String id : model.getRunways()){
-            paintRunway(id, g2);
-        }
-
-        for(String id : model.getRunways()){
-            paintCenterline(id, g2);
-        }
-    }
-
-    //Draws the clear and graded area for a given runway
-    private void paintClearAndGraded(String id, Graphics2D g2){
-        g2.setColor(new Color(80, 160, 79));
-        Point pos = model.getRunwayPos(id);
-        Dimension dim = model.getRunwayDim(id);
-
-        AffineTransform old = g2.getTransform();
-        AffineTransform tx = (AffineTransform) old.clone();
-        tx.concatenate(createRunwayTransform(pos,dim,id));
-        g2.setTransform(tx);
-
-        //Generate a polygon in the shape of the clear and graded area for the current runway.
-        g2.fillPolygon(genClearAndGradedPoly(pos, dim));
-        g2.setTransform(old);
-    }
-
-    //Draws the clear and graded area for all runways.
-    private void paintAllClearAndGraded(Graphics2D g2){
-        for(String id : model.getRunways()){
-            paintClearAndGraded(id, g2);
-        }
     }
 
     //Draws only the selected runway, such that it appears above all others and appears selected.
@@ -189,10 +131,101 @@ public class TopView2D extends JPanel {
             g2.setTransform(tx);
 
             //Drawing the highlight box.
-            g2.setColor(new Color(255, 165, 83));
-            g2.setStroke(new BasicStroke(SELECTED_RUNWAY_HIGHLIGHT_WIDTH));
+            g2.setColor(Settings.SELECTED_RUNWAY_HIGHLIGHT);
+            g2.setStroke(Settings.SELECTED_RUNWAY_STROKE);
             g2.drawRect(pos.x, pos.y, dim.width, dim.height);
+
             g2.setTransform(old);
+            paintStopway(selectedRunway, g2);
+            paintClearway(selectedRunway, g2);
+        }
+    }
+
+    //Configures the specified graphics object such that pan and zoom are taken into account.
+    private void configureGlobalTransform(Graphics2D g2){
+        //Create a global affine transformation which pans and zooms the view accordingly.
+        AffineTransform globalTransform = new AffineTransform();
+
+        //Translate and scale the view to match the pan and zoom settings.
+        globalTransform.translate(globalPan.x, globalPan.y);
+        globalTransform.scale(globalZoom,globalZoom);
+
+        //Set the create transformation to the one used by the image.
+        g2.setTransform(globalTransform);
+    }
+
+    //Draws a set of axis which intersect at (0,0).
+    private void paintAxis(Graphics2D g2){
+        g2.setColor(Settings.AXIS_COLOUR);
+        g2.setStroke(Settings.AXIS_STROKE);
+        g2.drawLine(-10000,0,10000,0);
+        g2.drawLine(0,-10000,0,10000);
+    }
+
+    //Draws a runway given the name.
+    private void paintRunway(String id, Graphics2D g2){
+        Point pos = model.getRunwayPos(id);
+        Dimension dim = model.getRunwayDim(id);
+
+        AffineTransform old = g2.getTransform();
+        AffineTransform tx = (AffineTransform) old.clone();
+        tx.concatenate(createRunwayTransform(pos,dim,id));
+        g2.setTransform(tx);
+
+        g2.setColor(Settings.RUNWAY_COLOUR);
+        g2.fillRect(pos.x, pos.y, dim.width, dim.height);
+        g2.setTransform(old);
+    }
+
+    //Draws the centerline for a runway given the name.
+    private void paintCenterline(String id, Graphics2D g2){
+        Point pos = model.getRunwayPos(id);
+        Dimension dim = model.getRunwayDim(id);
+
+        AffineTransform old = g2.getTransform();
+        AffineTransform tx = (AffineTransform) old.clone();
+        tx.concatenate(createRunwayTransform(pos,dim,id));
+        g2.setTransform(tx);
+
+        //Paint he centerline for the respective runway.
+        g2.setColor(Settings.CENTERLINE_COLOUR);
+        g2.setStroke(Settings.CENTERLINE_STROKE);
+        Integer padding = Settings.CENTERLINE_PADDING;
+        g2.drawLine(pos.x + padding, pos.y + dim.height/2, pos.x + dim.width - padding, pos.y + dim.height/2);
+        g2.setTransform(old);
+    }
+
+    //Draws the runway & centerline for all runways in the current model.
+    private void paintRunways(Graphics2D g2){
+        for(String id : model.getRunways()){
+            paintRunway(id, g2);
+        }
+
+        for(String id : model.getRunways()){
+            paintCenterline(id, g2);
+        }
+    }
+
+    //Draws the clear and graded area for a given runway
+    private void paintClearAndGraded(String id, Graphics2D g2){
+        Point pos = model.getRunwayPos(id);
+        Dimension dim = model.getRunwayDim(id);
+
+        AffineTransform old = g2.getTransform();
+        AffineTransform tx = (AffineTransform) old.clone();
+        tx.concatenate(createRunwayTransform(pos,dim,id));
+        g2.setTransform(tx);
+
+        //Generate a polygon in the shape of the clear and graded area for the current runway.
+        g2.setColor(Settings.CLEAR_AND_GRADED_COLOUR);
+        g2.fillPolygon(genClearAndGradedPoly(pos, dim));
+        g2.setTransform(old);
+    }
+
+    //Draws the clear and graded area for all runways.
+    private void paintAllClearAndGraded(Graphics2D g2){
+        for(String id : model.getRunways()){
+            paintClearAndGraded(id, g2);
         }
     }
 
@@ -252,9 +285,11 @@ public class TopView2D extends JPanel {
         @Override
         public void mouseWheelMoved(MouseWheelEvent e) {
             Integer scaleFactor = e.getWheelRotation();
-            if(scaleFactor < 0 & globalZoom * 1/0.95 < MAX_ZOOM_FACTOR ){
+            Double maxZoom = Settings.TOP_DOWN_MAX_ZOOM;
+            Double minZoom = Settings.TOP_DOWN_MIN_ZOOM;
+            if(scaleFactor < 0 & globalZoom * 1/0.95 < maxZoom ){
                 globalZoom = globalZoom * 1/0.95;
-            } else  if (scaleFactor > 0 & globalZoom * 0.95 > MIN_ZOOM_FACTOR){
+            } else  if (scaleFactor > 0 & globalZoom * 0.95 > minZoom){
                 globalZoom =  globalZoom * 0.95;
             } else {
                 return;
