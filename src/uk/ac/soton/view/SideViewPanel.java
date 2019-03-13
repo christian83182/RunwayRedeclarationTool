803ea04b05208a67d76c7c44d198100b3a911250
Ioana Moisoiu <moisoiuioana@yahoo.com>
2019-03-13 11:18:22 +0000

    Added gradient color from green to blue to the background

--- src/uk/ac/soton/view/SideViewPanel.java
+++ src/uk/ac/soton/view/SideViewPanel.java
@@ -1,6 +1,7 @@
 package uk.ac.soton.view;
 
 import java.awt.*;
+import java.awt.geom.NoninvertibleTransformException;
 
 public class SideViewPanel extends InteractivePanel{
 
@@ -13,7 +14,25 @@ public class SideViewPanel extends InteractivePanel{
 
     @Override
     public void paintView(Graphics2D g2) {
-        g2.setColor(Color.WHITE);
-        g2.fillRect(-50,-50,100,100);
+        paintGradientBackground(g2);
+
+    }
+
+    public void paintGradientBackground(Graphics2D g2){
+        Color colorBlue = new Color(84,122,179);
+        Color colorGreen = new Color(75,161,79);
+        GradientPaint gp = new GradientPaint(0, 0, colorBlue, 0, getHeight(), colorGreen);
+        g2.setPaint(gp);
+        Point topLeft = new Point(0,0);
+        Point bottomRight = new Point(getWidth(), getHeight());
+
+        try {
+            g2.getTransform().inverseTransform(topLeft, topLeft);
+            g2.getTransform().inverseTransform(bottomRight, bottomRight);
+            g2.fillRect(topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y - topLeft.y);
+        } catch (NoninvertibleTransformException e) {
+            e.printStackTrace();
+        }
+
     }
 }
