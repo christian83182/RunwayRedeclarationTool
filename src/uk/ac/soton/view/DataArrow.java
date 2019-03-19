8e1ece3ef0da44efef29e991c776bb406d5b324b
Christian Folkesson <christian.uaf@gmail.com>
2019-03-19 13:11:07 +0000

    Modified DataArrow to allow for vertically drawn arrows using a new method drawVerticalArrow().

--- src/uk/ac/soton/view/DataArrow.java
+++ src/uk/ac/soton/view/DataArrow.java
@@ -31,6 +31,80 @@ public class DataArrow {
     }
 
     /**
+     * Identical in every way to drawHorizontalArrow but the arrow is drawn vertically, with the helper lines coming out
+     * horizontally to the same x-coordinate.
+     * @param g2
+     */
+    public void drawVerticalArrow(Graphics2D g2){
+        Point arrowStart;
+        Point arrowEnd;
+        Integer padding = Settings.TOP_DOWN_INFO_ARROW_PADDING;
+
+        /*
+        Determine the position of the start and end of the arrow based on if the start point is above or
+        below the end point, and if the helper distance is positive of negative.
+         */
+        if(helperLength >= 0){
+            if (endPoint.x >= startPoint.x){
+                arrowStart = new Point(endPoint.x + helperLength,startPoint.y - padding);
+                arrowEnd = new Point(endPoint.x + helperLength, endPoint.y + padding);
+            } else {
+                arrowStart = new Point(startPoint.x + helperLength,startPoint.y - padding);
+                arrowEnd = new Point(startPoint.x + helperLength, endPoint.y + padding);
+            }
+        } else {
+            if (endPoint.x >= startPoint.x){
+                arrowStart = new Point(startPoint.x + helperLength,startPoint.y - padding);
+                arrowEnd = new Point(startPoint.x + helperLength, endPoint.y + padding);
+            } else {
+                arrowStart = new Point(endPoint.x + helperLength,startPoint.y - padding);
+                arrowEnd = new Point(endPoint.x + helperLength, endPoint.y + padding);
+            }
+        }
+
+        //Draw the dotted "helper" lines.
+        g2.setColor(Settings.INFO_ARROW_COLOUR);
+        g2.setStroke(Settings.INFO_ARROW_HELPER_STROKE);
+
+        g2.drawLine(startPoint.x, startPoint.y, arrowStart.x, arrowStart.y + padding);
+        g2.drawLine(endPoint.x, endPoint.y , arrowEnd.x, arrowEnd.y - padding);
+
+        //Draw the solid line which will becomes the arrow.
+        Integer arrowLength = Settings.TOP_DOWN_INFO_ARROW_LENGTH;
+        Integer arrowHeight = Settings.TOP_DOWN_INFO_ARROW_HEIGHT;
+        g2.setStroke(Settings.INFO_ARROW_STROKE);
+        g2.drawLine(arrowStart.x , arrowStart.y - arrowLength, arrowEnd.x , arrowEnd.y + arrowLength);
+
+        //Create two polygons for the arrow heads at each end of the line.
+        Polygon arrowHeadStart = new Polygon(new int[] {arrowStart.x - arrowHeight, arrowStart.x , arrowStart.x + arrowHeight},
+                new int[] {arrowStart.y -arrowLength, arrowStart.y , arrowStart.y - arrowLength}, 3);
+        Polygon arrowHeadEnd = new Polygon(new int[] {arrowEnd.x - arrowHeight, arrowEnd.x , arrowEnd.x + arrowHeight},
+                new int[] {arrowEnd.y +arrowLength, arrowEnd.y , arrowEnd.y + arrowLength}, 3);
+
+        //Draw the two arrowheads
+        g2.fillPolygon(arrowHeadStart);
+        g2.fillPolygon(arrowHeadEnd);
+
+        //Set the font to the correct font specified in the settings class.
+        g2.setFont(Settings.INFO_TEXT_FONT);
+        FontMetrics fontMetrics = g2.getFontMetrics();
+
+        //Using a font metrics object gained from g2, determine the width and height of the label.
+        Integer textPadding = Settings.TOP_DOWN_INFO_TEXT_PADDING;
+        Integer stringLength = g2.getFontMetrics().stringWidth(label);
+        Integer stringHeight = Settings.INFO_TEXT_FONT.getSize();
+
+        g2.setColor(Settings.INFO_TEXT_COLOUR);
+        g2.setFont(Settings.INFO_TEXT_FONT);
+
+        if(helperLength > 0){
+            g2.drawString(label,arrowStart.x + textPadding,(startPoint.y + endPoint.y + stringHeight)/2 -5);
+        } else {
+            g2.drawString(label,arrowStart.x - stringLength - textPadding,(startPoint.y + endPoint.y + stringHeight)/2 -5);
+        }
+    }
+
+    /**
      * Takes some Graphics2D object and draws a data arrow according to the properties of the object.
      * Dotted lines are drawn vertically down from the start and end point, and terminate at the same y-level.
      * A solid line with arrow heads at each end is drawn horizontally between the two dotted lines.
@@ -48,7 +122,7 @@ public class DataArrow {
      * @param g2 The graphics object used to draw the arrow. Note that the graphics object is not modifed in any way,
      * and is used as is.
      */
-    public void drawArrow(Graphics2D g2){
+    public void drawHorizontalArrow(Graphics2D g2){
         Point arrowStart;
         Point arrowEnd;
         Integer padding = Settings.TOP_DOWN_INFO_ARROW_PADDING;
