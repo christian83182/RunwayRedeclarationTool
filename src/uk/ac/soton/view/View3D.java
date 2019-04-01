5149b77a65933b208e285ab583271d9ae3c19281
Christian Folkesson <christian.uaf@gmail.com>
2019-04-01 23:36:52 +0100

    View3D now displays a basic version of the airfield given by the controller.

--- src/uk/ac/soton/view/View3D.java
+++ src/uk/ac/soton/view/View3D.java
@@ -4,18 +4,16 @@ import javafx.application.Platform;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.embed.swing.JFXPanel;
-import javafx.scene.Group;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Transform;
 import uk.ac.soton.controller.ViewController;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class View3D extends JFrame{
 
@@ -24,8 +22,8 @@ public class View3D extends JFrame{
     private double startX, startY;
     private double anchorAngleX = 0;
     private double anchorAngleY = 0;
-    private final DoubleProperty angleX = new SimpleDoubleProperty(0);
-    private final DoubleProperty angleY = new SimpleDoubleProperty(0);
+    private final DoubleProperty angleX = new SimpleDoubleProperty(60);
+    private final DoubleProperty angleY = new SimpleDoubleProperty(45);
 
     View3D(AppView appView){
         super("3D Visualization");
@@ -51,9 +49,9 @@ public class View3D extends JFrame{
 
     //Initializes the JavaFX content for the JFXPanel. Should run in a separate thread from the EDT.
     private void initJFX(JFXPanel fxPanel) {
-        GroupRotate root = new GroupRotate();
+        Group root = new Group();
         Scene scene = new Scene(root, getWidth(), getHeight(), true);
-        scene.setFill(Color.SILVER);
+        scene.setFill(convertToJFXColour(Settings.AIRFIELD_COLOUR));
         initCamera(scene, root);
         createScene(scene, root);
         fxPanel.setScene(scene);
@@ -61,18 +59,16 @@ public class View3D extends JFrame{
 
     //Creates the JavaFx scene which should be contained within the JFXPanel.
     private void createScene(Scene scene, Group root) {
-
-        Box box = new Box(100,100,100);
-        box.setTranslateY(-50);
-
-        Box plane = new Box(5000,1,5000);
-        plane.setMaterial(new PhongMaterial(Color.LIMEGREEN));
-
-        root.getChildren().addAll(box, plane);
-
+        if(appView.getSelectedRunway().equals("")){
+            generateRunways(root);
+        } else if(appView.getMenuPanel().isIsolateMode()){
+            //draw only one one runway, focus on one.
+        } else {
+            //draw all runways, focus on one.
+        }
     }
 
-    private void initCamera(Scene scene, GroupRotate root){
+    private void initCamera(Scene scene, Group root){
         PerspectiveCamera camera = new PerspectiveCamera(true);
         scene.setCamera(camera);
 
@@ -80,8 +76,6 @@ public class View3D extends JFrame{
         camera.setNearClip(1);
         camera.setFarClip(5000);
         camera.setTranslateZ(-200);
-
-        root.rotateByX(90);
 
         Rotate xRotate = new Rotate(0, Rotate.X_AXIS);
         Rotate yRotate = new Rotate(0, Rotate.Y_AXIS);
@@ -99,10 +93,10 @@ public class View3D extends JFrame{
 
         scene.setOnMouseDragged(event -> {
             Double newAngleX = anchorAngleX - (startY - event.getSceneY())*0.2;
-            if(newAngleX < -85){
-                angleX.set(-85);
-            } else if (newAngleX > 0){
-                angleX.set(0);
+            if(newAngleX > 90){
+                angleX.set(90);
+            } else if (newAngleX < 5){
+                angleX.set(5);
             } else {
                 angleX.set(newAngleX);
             }
@@ -119,24 +113,62 @@ public class View3D extends JFrame{
         });
     }
 
-    class GroupRotate extends Group {
-        Rotate r;
-        Transform t = new Rotate();
-
-        void rotateByX(int ang) {
-            r = new Rotate(ang, Rotate.X_AXIS);
-            t = t.createConcatenation(r);
-            this.getTransforms().clear();
-            this.getTransforms().addAll(t);
-        }
-
-        void rotateByY(int ang) {
-            r = new Rotate(ang, Rotate.Y_AXIS);
-            t = t.createConcatenation(r);
-            this.getTransforms().clear();
-            this.getTransforms().addAll(t);
-        }
+    private Color convertToJFXColour(java.awt.Color swingColour){
+        double red = swingColour.getRed()/255.0;
+        double green = swingColour.getGreen()/255.0;
+        double blue = swingColour.getBlue()/255.0;
+        double opacity = swingColour.getAlpha() / 255.0;
+        return new Color(red,green, blue,opacity);
     }
 
+    private void generateRunwayStrip(Group root, String runwayId){
+        Point pos = controller.getRunwayPos(runwayId);
+        Dimension dim = controller.getRunwayDim(runwayId);
+        Integer distanceFromCenterline = controller.getStripWidthFromCenterline(runwayId);
+        Integer stripEndSize = controller.getStripEndSize(runwayId);
+
+        Box stripBox = new Box(distanceFromCenterline*2,12,dim.width+stripEndSize*2);
+        PhongMaterial stripMaterial = new PhongMaterial(convertToJFXColour(Settings.RUNWAY_STRIP_COLOUR));
+        stripBox.setMaterial(stripMaterial);
+
+        stripBox.setTranslateX(pos.x);
+        stripBox.setTranslateZ(-pos.y + dim.width/2.0);
+
+        Rotate r = new Rotate(controller.getBearing(runwayId), 0,0,-dim.width/2.0, Rotate.Y_AXIS);
+        stripBox.getTransforms().add(r);
+
+        root.getChildren().add(stripBox);
+    }
+
+    private void generateRunway(Group root, String runwayId){
+        Point pos = controller.getRunwayPos(runwayId);
+        Dimension dim = controller.getRunwayDim(runwayId);
+
+        Box runwayBox = new Box(dim.height,20,dim.width);
+        PhongMaterial runwayMaterial = new PhongMaterial(convertToJFXColour(Settings.RUNWAY_COLOUR));
+        runwayBox.setMaterial(runwayMaterial);
+
+        runwayBox.setTranslateX(pos.x);
+        runwayBox.setTranslateZ(-pos.y + dim.width/2.0);
+
+        Rotate r = new Rotate(controller.getBearing(runwayId), 0,0,-dim.width/2.0, Rotate.Y_AXIS);
+        runwayBox.getTransforms().add(r);
+
+        root.getChildren().add(runwayBox);
+    }
+
+    private void generateRunways(Group root){
+        ArrayList<Integer> drawnRunways = new ArrayList<>();
+
+        for(String runwayId : controller.getRunways()){
+            Integer currentRunwayBearing = controller.getBearing(runwayId);
+            if(!(drawnRunways.contains(currentRunwayBearing+180) || drawnRunways.contains(currentRunwayBearing-180))){
+
+                generateRunway(root, runwayId);
+                generateRunwayStrip(root, runwayId);
+                drawnRunways.add(currentRunwayBearing);
+            }
+        }
+    }
 }
 
